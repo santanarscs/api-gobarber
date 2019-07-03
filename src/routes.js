8bcd561d32d7f63faa2a2f3f31bea576cccc9d56
santanarscs <raphaelstn@gmail.com>
2019-07-03 14:26:16 -0300

    config models user

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Raphael santnaa',
+    email: 'raphaelstn@gmail.com',
+    password_hash: '123123',
+  });
+
+  res.json(user);
+});
 
 export default routes;

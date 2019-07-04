5647c5b65a283d29b1824da127ba156c21f2b311
santanarscs <raphaelstn@gmail.com>
2019-07-04 15:54:12 -0300

    update notification

--- src/routes.js
+++ src/routes.js
@@ -24,5 +24,6 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/schedule', ScheduleController.index);
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;

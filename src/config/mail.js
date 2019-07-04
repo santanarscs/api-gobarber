508d331582366cdb73c81121b90f9be8058b9c00
santanarscs <raphaelstn@gmail.com>
2019-07-04 16:16:12 -0300

    configure send mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '	26b11844604e23',
+    pass: 'd36ccc7102d6be',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};

b1ad86e7fd18f03f97f9c18ff60589cb77b17388
santanarscs <raphaelstn@gmail.com>
2019-07-16 10:13:31 -0300

    configure sentry

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.excepitionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -20,6 +29,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  excepitionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://54a4c31eba1f4aa8a04b4e8e55b487d8@sentry.io/1506066',
+};
